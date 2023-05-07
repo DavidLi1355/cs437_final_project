7c2e6affd45a52000b5ba26fc24c4d1231351cc3
David Li <lid1355218215@outlook.com>
2023-05-07 16:50:43 -0500

    fix multiprocessing

--- main.py
+++ main.py
@@ -8,34 +8,34 @@ from src.music import Music
 import threading
 from server import run_server
 
-# setup root
-root = Tk()
-root.geometry("700x900")
-root.resizable(False, False)
-root.title("Clock")
+if __name__ == "__main__":
+    # setup root
+    root = Tk()
+    root.geometry("700x900")
+    root.resizable(False, False)
+    root.title("Clock")
 
-# setup clock and alarm
+    # setup clock and alarm
 
-time_display = TimeDisplay(root, is_client=False)
-time_display.get_time()
-alarm = Alarm(root, is_client=False)
-news = News(root)
-music = Music(root)
-speech_to_text = SpeechToText(alarm, news, music)
+    time_display = TimeDisplay(root, is_client=False)
+    time_display.get_time()
+    alarm = Alarm(root, is_client=False)
+    news = News(root)
+    music = Music(root)
+    speech_to_text = SpeechToText(alarm, news, music)
 
-my_thread = threading.Thread(
-    target=run_server,
-    args=(
-        time_display,
-        alarm,
-    ),
-)
-my_thread.start()
+    my_thread = threading.Thread(
+        target=run_server,
+        args=(
+            time_display,
+            alarm,
+        ),
+    )
+    my_thread.start()
 
-
-mainloop()
-print("Hi")
-# Wait for the thread to complete
-my_thread.join()
-# clean up thread
-del alarm
+    mainloop()
+    print("Hi")
+    # Wait for the thread to complete
+    my_thread.join()
+    # clean up thread
+    del alarm
