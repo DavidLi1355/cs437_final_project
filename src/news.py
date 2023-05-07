13f697972f9098528ab66293e9af4d186f67d6aa
David Li <lid1355218215@outlook.com>
2023-05-07 15:28:06 -0500

    add more clean up

--- src/news.py
+++ src/news.py
@@ -3,14 +3,16 @@ import gtts
 from playsound import playsound
 from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen
-from threading import Thread, Condition
+from threading import Thread, Event
 from queue import Queue
 
 
 class NewsHandler:
     @staticmethod
-    def read_news_handler(work_queue: Queue):
+    def read_news_handler(work_queue: Queue, event: Event):
         while True:
+            if event.isSet():
+                break
             news_to_read: str = work_queue.get()
             tts = gtts.gTTS(news_to_read)
             tts.save("news.mp3")
@@ -34,11 +36,21 @@ class News:
         self.soup_page = soup(self.xml_page, "xml")
         self.news_list = self.soup_page.findAll("item")
 
+        self.event = Event()
         self.work_queue = Queue()
         self.read_thread = Thread(
-            target=NewsHandler.read_news_handler, args=(self.work_queue,)
+            target=NewsHandler.read_news_handler,
+            args=(
+                self.work_queue,
+                self.event,
+            ),
         )
         self.read_thread.start()
+
+    def __del__(self):
+        self.event.set()
+        self.work_queue.put("")
+        self.read_thread.join()
 
     def read_news_callback(self):
         news_to_read = ""
