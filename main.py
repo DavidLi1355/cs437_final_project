d41e62e8c351143b2203bbe6bec407c1f37809a3
David Li <lid1355218215@outlook.com>
2023-05-07 15:21:35 -0500

    added music and improved layout

--- main.py
+++ main.py
@@ -3,33 +3,41 @@ from src.time_display import TimeDisplay
 from src.alarm import Alarm
 from src.speech_to_text import SpeechToText
 from src.news import News
+from src.music import Music
 
 import threading
 from server import run_server
-# setup root
-root = Tk()
-root.geometry("700x900")
-root.resizable(False, False)
-root.title("Clock")
-
-# setup clock and alarm
-
-time_display = TimeDisplay(root, is_client=False)
-time_display.get_time()
-alarm = Alarm(root, is_client=False)
-
-news = News(root)
-speech_to_text = SpeechToText(alarm)
-
-my_thread = threading.Thread(target=run_server, args=(time_display, alarm,))
-my_thread.start()
 
 
+if __name__ == "__main__":
+    # setup root
+    root = Tk()
+    root.geometry("700x900")
+    root.resizable(False, False)
+    root.title("Clock")
 
+    # setup clock and alarm
+    time_display = TimeDisplay(root, is_client=False)
+    time_display.get_time()
+    alarm = Alarm(root, is_client=False)
+    # setup news and music
+    news = News(root)
+    music = Music(root)
+    # setup text to speech control
+    speech_to_text = SpeechToText(alarm)
 
-mainloop()
-print("Hi")
-# Wait for the thread to complete
-my_thread.join()
-# clean up thread
-del alarm
+    my_thread = threading.Thread(
+        target=run_server,
+        args=(
+            time_display,
+            alarm,
+        ),
+    )
+    my_thread.start()
+
+    mainloop()
+    print("Hi")
+    # Wait for the thread to complete
+    my_thread.join()
+    # clean up thread
+    del alarm
